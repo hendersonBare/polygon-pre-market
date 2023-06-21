17d8db3fb5c0cfe7636a59e4473cbb743ec35ca6
hendersonBare <henderson.bare@gmail.com>
2023-06-20 19:47:26 -0600

    established a working connection and was able to print html from webpage

--- webscrape.py
+++ webscrape.py
@@ -7,14 +7,15 @@ used sparingly since there are a wide range of errors that can arrise from relyi
 web scraping
 """
 
-import selenium, config
+import selenium, config, bs4
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 Options = webdriver.ChromeOptions()
 Options.headless = True
 #options below copied from a youtube video, hopefully solves current errors
-Options.add_argument(f'user-agent={user_agent}')
+#Options.add_argument(f'user-agent={user_agent}')
 Options.add_argument("--window-size=1920,1080")
 Options.add_argument('--ignore-certificate-errors')
 Options.add_argument('--allow-running-insecure-content')
@@ -25,9 +26,20 @@ Options.add_argument("--start-maximized")
 Options.add_argument('--disable-gpu')
 Options.add_argument('--disable-dev-shm-usage')
 Options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path="chromedriver.exe", options=Options)
+driver = webdriver.Chrome(options=Options)
 
 webURL = driver.get("https://www.slickcharts.com/sp500")
-driver.quit()
 
-print(webURL);
+html_content = driver.page_source
+
+# Parse the HTML using BeautifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find all the <td> elements containing ticker symbols
+ticker_elements = soup.find_all('td', {'class': ''})
+
+# Extract the ticker symbols and add them to a list
+ticker_symbols = [element.text for element in ticker_elements if element.text.isalpha()]
+
+
+print(html_content);
