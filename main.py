b2de2ebb01cde9b8dd1dcc7435df39b1a7862541
hendersonBare <henderson.bare@gmail.com>
2023-06-21 18:55:39 -0600

    added to the isValidTicker method to find the previous close price of a desired stock, also added documentation and TODOs regarding error handling

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
+#NOTE: Polygon API calls only return data for stocks listed on US stock exchanges.
+
 import config, json, requests, datetime, pandas
 
 def isValidTicker(ticker):
-    response = requests.get('https://api.polygon.io/v2/aggs/ticker/' + ticker + '/range/1/minute' +
-                        '/1686888000000/1686922200000?adjusted=true&sort=asc&limit=5000&apiKey=' + config.API_KEY)
-    data = response.json()
-    #need error handling for when the ticker yeilds no data or response
+    openResponse = requests.get('https://api.polygon.io/v1/open-close/' + ticker + '/2021-07-30?adjusted=true&apiKey=' + config.API_KEY)
+
+    openResponseJSON = openResponse.json()
+    """TODO: Error handling. Possible error cases:
+       1.) Stock is listed on an exchange outside of the US
+       2.) The stock ticker has changed """
     try:
-        if data['resultsCount'] <= 0:
+        open = openResponseJSON['open']
+    except:
+        print("an error occured trying to find the open price")
+    #API call that returns all premarket data in the form of one minute candles for a certain ticker
+    response = requests.get('https://api.polygon.io/v2/aggs/ticker/' + ticker + '/range/1/minute' +
+                        '/1686888000000/1686922200000?adjusted=true&sort=asc&limit=5000&apiKey=' + config.API_KEY) 
+    data = response.json()
+    #TODO: need error handling for when the ticker yeilds no data or response
+    try:
+        if data['resultsCount'] <= 0: #'resultsCount' corresponds to the number of candles returned
             return
     except:
-        print("an error occured")
-    aggregates = data['results']
-    open = aggregates[000]['o']
+        print("an error occured trying to get premarket candles")
+    aggregates = data['results'] #the list of candles 
+    #open = aggregates[000]['o'] 
     threshold = open * 1.2
     for r in aggregates:
         if r['h'] > threshold:
             print(ticker)
             return ticker
 
-"""response = requests.get('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute' +
-                        '/1686888000000/1686922200000?adjusted=true&sort=asc&limit=5000&apiKey=' + config.API_KEY)
+#isValidTicker("BRK.AWKND") ##test case for more "obscure" tickers
 
-data = response.json() # converts the HTTP response into a JSON object
-
-results = data['results'] # creates a list of the 'results' in this JSON object to ignore unneeded info
-
-for x in results:
-    print(datetime.datetime.fromtimestamp(x['t'] / 1000))
-
-print('\n')
-print(datetime.datetime.fromtimestamp(1686888000000 / 1000))
-
-print(datetime.datetime.fromtimestamp(1686907800000 / 1000))
-
-for x in results:
-    print (x['h']) """
-
-isValidTicker("BRK.AWKND") ##test case for more "obscure" tickers
-
-for y in range(0, len(config.tickers)):
-    isValidTicker(config.tickers[y])
+for y in range(0, len(config.ConfirmationList)):
+    isValidTicker(config.ConfirmationList[y])
 
