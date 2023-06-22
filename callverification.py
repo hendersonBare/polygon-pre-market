dfef7e63dc34023c3328a700862ea5fcd4937546
hendersonBare <henderson.bare@gmail.com>
2023-06-21 18:56:55 -0600

    added testing code to a seperate file. testing code is pretty ugly and is done manually

---
+++ callverification.py
@@ -0,0 +1,21 @@
+# a seperate file used to manually verify API calls and the information that they put out
+
+import config, json, requests, datetime, pandas
+
+response = requests.get('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute' +
+                        '/1686888000000/1686922200000?adjusted=true&sort=asc&limit=5000&apiKey=' + config.API_KEY)
+
+data = response.json() # converts the HTTP response into a JSON object
+
+results = data['results'] # creates a list of the 'results' in this JSON object to ignore unneeded info
+
+for x in results:
+    print(datetime.datetime.fromtimestamp(x['t'] / 1000))
+
+print('\n')
+print(datetime.datetime.fromtimestamp(1627610400000 / 1000))
+
+print(datetime.datetime.fromtimestamp(1686907800000 / 1000))
+
+"""for x in results:
+    print (x['h'])"""
