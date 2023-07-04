cf21a3c02000e23d5e972cc1cb9ed348134ce446
hendersonBare <henderson.bare@gmail.com>
2023-07-04 14:43:16 -0600

    created a rough outline for date iterations

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import TickerFinder, config
+import TickerFinder, config, datetime
 file = open('NASDAQ_screener_tickers.txt', "r")
 
 lines = file.readlines()
@@ -7,6 +7,15 @@ NASDAQ_Tickers = [line.strip() for line in lines]
 
 file.close()
 
-for y in range(0, len(NASDAQ_Tickers)):
-    TickerFinder.isValidTicker(NASDAQ_Tickers[y])
+#rough iteration through dates
+
+date = datetime.datetime.strptime("2023-06-30", "%Y-%m-%d")
+date = date - datetime.timedelta(days=1)
+dateString = date.strftime("%Y-%m-%d")
+
+for x in range(0, 3):
+    date = date - datetime.timedelta(days=1)
+    dateString = date.strftime("%Y-%m-%d")
+    for y in range(0, len(NASDAQ_Tickers)):
+        TickerFinder.isValidTicker(NASDAQ_Tickers[y], dateString)
 
