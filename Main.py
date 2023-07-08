7bb98dc6196c897b721fb057e8da8526facb7c1e
hendersonBare <henderson.bare@gmail.com>
2023-07-08 00:34:45 -0600

    fixed formatting and added documentation

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import TickerFinder, config, datetime, pyodbc
 connection_string = 'DRIVER={SQL Server};SERVER=' + config.server + ';DATABASE=' + config.database + ';Trusted_Connection=yes;'
 
 connection = pyodbc.connect(connection_string)
+cursor = connection.cursor()
 
 file = open('NASDAQ_screener_tickers.txt', "r")
 
@@ -22,5 +23,7 @@ for x in range(0, 3):
     date = date - datetime.timedelta(days=1)
     dateString = date.strftime("%Y-%m-%d")
     for y in range(0, len(NASDAQ_Tickers)):
-        TickerFinder.isValidTicker(NASDAQ_Tickers[y], dateString)
+        TickerFinder.isValidTicker(NASDAQ_Tickers[y], dateString, cursor)
 
+connection.close()
+cursor.close()
